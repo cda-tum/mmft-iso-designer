41c757be3e30ffc7be84172761ff20a3fafc2ab9
philippebner <29914922+philippebner@users.noreply.github.com>
2024-05-17 12:33:26 +0200

    Remove obsolete code

--- src/da/design.ts
+++ src/da/design.ts
@@ -24,21 +24,8 @@ async function design(input: Input) {
         solver.set('unsat_core', false)
 
 
-        const encoded_input = input.encode(ctx)
-
-        /*const [x, y, z] = ctx.Int.consts('x y z')
-        const p1 = ctx.LE(x, y)
-        const p2 = ctx.LE(y, z)
-        const p3 = ctx.LT(z, x)
-        solver.add(p1)
-        solver.add(p2)
-        solver.add(p3)*/
-
-        
+        const encoded_input = input.encode(ctx) 
         solver.add(...encoded_input.clauses)
-        //solver.add(ctx.BitVec.const('ebb_0_rotation', 2).eq(ctx.BitVec.val(3, 2)))
-
-        //;[...solver.assertions().values()].forEach(f => console.log(f.toString()))
 
         let start = performance.now()
         const check = await solver.check()
@@ -52,67 +39,12 @@ async function design(input: Input) {
                 success: false,
                 timing: number
             }
-            /*const avec = Z3.solver_get_unsat_core(ctx.ptr, solver.ptr)
-            const size = Z3.ast_vector_size(ctx.ptr, avec)
-            const asts = [...Array(size).keys()].map(i => Z3.ast_vector_get(ctx.ptr, avec, i))
-            console.log(size)
-            console.log(asts)*/
         } else if (check === 'unknown') {
             throw 'Z3 cannot determine whether there is a solution.'
         } else {
-            console.log('sat')
             const model = solver.model()
             const result = encoded_input.result(model)
             result.timing = timing
-            console.log(result)
-            /*console.log(model.eval(encoded_input.channels[3].segments[0].type.bitvector).toString())
-            console.log(model.eval(channel_segments_no_cross(ctx, encoded_input.channels[3], 0, encoded_input.channels[2], 1)).toString(), result.channels[2], result.channels[3])
-            console.log(model.eval(ctx.Implies(
-                ctx.And(
-                    encoded_input.channels[3].segments[0].type.eq(ctx, SegmentType.Up),
-                    encoded_input.channels[2].segments[0].type.eq(ctx, SegmentType.Right)
-                ),
-                segment_segment_no_cross(ctx, {
-                    c1_lower: encoded_input.channels[3].waypoints[0].y,
-                    c1_higher: encoded_input.channels[3].waypoints[0 + 1].y,
-                    c2:  encoded_input.channels[3].waypoints[0].x
-                }, {
-                    c1: encoded_input.channels[2].waypoints[0].y,
-                    c2_lower: encoded_input.channels[2].waypoints[0].x,
-                    c2_higher: encoded_input.channels[2].waypoints[0 + 1].x,
-                })
-            ),).toString())
-            console.log("3 is up", model.eval(encoded_input.channels[3].segments[0].type.eq(ctx, SegmentType.Up)).toString())
-            console.log("2 is right", model.eval(encoded_input.channels[2].segments[0].type.eq(ctx, SegmentType.Right)).toString())
-            console.log("no cross", model.eval(segment_segment_no_cross(ctx, {
-                c1_lower: encoded_input.channels[3].waypoints[0].y,
-                c1_higher: encoded_input.channels[3].waypoints[0 + 1].y,
-                c2:  encoded_input.channels[3].waypoints[0].x
-            }, {
-                c1: encoded_input.channels[2].waypoints[0].y,
-                c2_lower: encoded_input.channels[2].waypoints[0].x,
-                c2_higher: encoded_input.channels[2].waypoints[0 + 1].x,
-            })).toString())
-            console.log("no cross", model.eval(segment_segment_no_cross(ctx, {
-                c1_lower: ctx.Int.val(9000),
-                c1_higher: ctx.Int.val(18000),
-                c2: ctx.Int.val(12000)
-            }, {
-                c1: ctx.Int.val(12000),
-                c2_lower: ctx.Int.val(3000),
-                c2_higher: ctx.Int.val(42000),
-            })).toString())
-            console.log(
-                model.eval(encoded_input.channels[3].waypoints[0].y).toString(), 
-                model.eval(encoded_input.channels[3].waypoints[0 + 1].y).toString(), 
-                model.eval(encoded_input.channels[3].waypoints[0].x).toString(), 
-                model.eval(encoded_input.channels[2].waypoints[0].y).toString(), 
-                model.eval(encoded_input.channels[2].waypoints[0].x).toString(), 
-                model.eval(encoded_input.channels[2].waypoints[0 + 1].x).toString())
-            console.log(model.eval(ctx.Implies(ctx.And(
-                encoded_input.channels[3].segments[0].type.eq(ctx, SegmentType.Up),
-                encoded_input.channels[2].segments[0].type.eq(ctx, SegmentType.Right)
-            ), true)).toString())*/
             return result
         }
     } catch (e) {
