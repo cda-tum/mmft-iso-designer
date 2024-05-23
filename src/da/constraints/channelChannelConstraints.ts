9827adb55360537b2e007a7c7126810cf5076ddb
philippebner <29914922+philippebner@users.noreply.github.com>
2024-05-23 15:52:45 +0200

    Change to camelCase

--- src/da/constraints/channelChannelConstraints.ts
+++ src/da/constraints/channelChannelConstraints.ts
@@ -21,28 +21,28 @@ export function encodeChannelChannelConstraints(ctx: Context, a: EncodedChannelI
 
     /* Specify waypoint distance to waypoints of other channels */
     {
-        const min_distance = Math.ceil((a.width + b.width) / 2 + Math.max(a.spacing, b.spacing))
+        const minDistance = Math.ceil((a.width + b.width) / 2 + Math.max(a.spacing, b.spacing))
         clauses.push(...cross(a.waypoints, b.waypoints).map(([wa, wb]) => {
             return ctx.Or(
-                minDistanceSym(ctx, wa.x, wb.x, min_distance),
-                minDistanceSym(ctx, wa.y, wb.y, min_distance)
+                minDistanceSym(ctx, wa.x, wb.x, minDistance),
+                minDistanceSym(ctx, wa.y, wb.y, minDistance)
             )
         }))
     }
 
     /* Specify waypoint distance to segments of other channels */
     {
-        const min_distance = Math.ceil((a.width + b.width) / 2 + Math.max(a.spacing, b.spacing))
+        const minDistance = Math.ceil((a.width + b.width) / 2 + Math.max(a.spacing, b.spacing))
         clauses.push(...cross([...a.waypoints.keys()], [...b.segments.keys()]).map(([wa, sb]) => {
             return ctx.Implies(
                 b.segments[sb].active,
-                waypointSegmentDistance(ctx, a, wa, b, sb, min_distance),
+                waypointSegmentDistance(ctx, a, wa, b, sb, minDistance),
             )
         }))
         clauses.push(...cross([...a.segments.keys()], [...b.waypoints.keys()]).map(([sa, wb]) => {
             return ctx.Implies(
                 a.segments[sa].active,
-                waypointSegmentDistance(ctx, b, wb, a, sa, min_distance)
+                waypointSegmentDistance(ctx, b, wb, a, sa, minDistance)
             )
         }))
     }
