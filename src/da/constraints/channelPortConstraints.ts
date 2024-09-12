24d0302b11d37bdd082546afcf5f534c98b6fcff
tim-scholl <tim.scholl@live.de>
2024-09-12 18:45:57 +0200

    added placement and port logic
    Signed-off-by: tim-scholl <tim.scholl@live.de>

--- src/da/constraints/channelPortConstraints.ts
+++ src/da/constraints/channelPortConstraints.ts
@@ -5,15 +5,18 @@ import { EncodedChannel } from "../channel";
 export function encodeChannelPortConstraints(ctx: Context, channel: EncodedChannel, fromModule: EncodedModule, toModule: EncodedModule) {
     const clauses = []
 
-    const fromPortPosition = fromModule.portPosition(ctx, channel.from.port[0], channel.from.port[1])
-    const toPortPosition = toModule.portPosition(ctx, channel.to.port[0], channel.to.port[1])
+    if (toModule.placement == fromModule.placement) {
+        const fromPortPosition = fromModule.portPosition(ctx, channel.from.port[0], channel.from.port[1])
+        const toPortPosition = toModule.portPosition(ctx, channel.to.port[0], channel.to.port[1])
 
-    clauses.push(
-        ctx.Eq(channel.encoding.waypoints[0].x, fromPortPosition.x),
-        ctx.Eq(channel.encoding.waypoints[0].y, fromPortPosition.y),
-        ctx.Eq(channel.encoding.waypoints[channel.maxSegments].x, toPortPosition.x),
-        ctx.Eq(channel.encoding.waypoints[channel.maxSegments].y, toPortPosition.y),
-    )
-
+        clauses.push(
+            ctx.Eq(channel.encoding.waypoints[0].x, fromPortPosition.x),
+            ctx.Eq(channel.encoding.waypoints[0].y, fromPortPosition.y),
+            ctx.Eq(channel.encoding.waypoints[channel.maxSegments].x, toPortPosition.x),
+            ctx.Eq(channel.encoding.waypoints[channel.maxSegments].y, toPortPosition.y),
+        )
+    } else {
+        clauses.push(ctx.Bool.val(false))
+    }
     return clauses
 }
